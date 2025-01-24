3c92d99c20fb1cff96d50252d373ed3ea22b076e
Zafarbek <z.anvarzoda@newuu.uz>
2025-01-24 12:08:36 +0500

    5 Tasks

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+// int main() {
+//     string name;
+//     int age;
+//     cout << "Please enter your name: ";
+//     cin >> name;
+//     cout << "Please enter your age: ";
+//     cin >> age;
+//
+//     cout << "Your name is " << name << " you are " << age << " years old" << std::endl;
+//
+//     return 0;
+//
+// }
+
+
+// using namespace std;
+// int main() {
+//     cout << setw(10) << "1" <<endl;
+//     cout << setw(5) << "2" <<endl;
+//     cout << setw(5) << "3" <<endl;
+//     cout << setw(5) << "4" <<endl;
+//     cout << setw(5) << "5" <<endl;
+//     return 0;
+// }
+
+
+//3
+// int main() {
+//     int a;
+//     int b;
+//     cout << "Please set number for a: ";
+//     cin >> a;
+//     cout << "Please set number for b: ";
+//     cin >> b;
+//     swap(a, b);
+//     cout << a << " " << b << endl;
+//
+// }
+
+//4
+// int main() {
+//     int n;
+//     cout << "Please enter number: ";
+//     cin >> n;
+//     for (int i = 1; i < n+1; i++) {
+//         cout << i*i << endl;
+//     }
+// }
+
+
+
+ // int main() {
+ //     string text;
+ //     cout << "Please write some text: ";
+ //     cin >> text;
+ //     cout << setw(50) << text << endl;
+ //
+ // }
